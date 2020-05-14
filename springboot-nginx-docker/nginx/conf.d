1819a9c12e4a64e7a817e0e07ee8b2a438ecd3f3
Kumar <adarshkumarsingh83@gmail.com>
2020-05-14 16:23:27 -0500

    springboot & ngix working

--- springboot-nginx-docker/nginx/conf.d
+++ springboot-nginx-docker/nginx/conf.d
@@ -1,10 +1,14 @@
+upstream springboot-app {
+    server springboot-app;
+}
+
 server {
     listen 80;
     charset utf-8;
     access_log off;
 
     location / {
-        proxy_pass http://app:9090;
+        proxy_pass http://springboot-app:9090/;
         proxy_set_header Host $host:$server_port;
         proxy_set_header X-Forwarded-Host $server_name;
         proxy_set_header X-Real-IP $remote_addr;
@@ -14,7 +18,6 @@ server {
     location /static {
         access_log   off;
         expires      30d;
-
         alias /app/static;
     }
 }

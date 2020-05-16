d36fe53da60aa0f1a2e37ec88e92dd21f6ea2034
Kumar <adarshkumarsingh83@gmail.com>
2020-05-16 18:24:44 -0500

    adding working code

--- springboot-jpa-mysql-docker/src/test/java/com/espark/adarsh/ApplicationMainTests.java
+++ springboot-jpa-mysql-docker/src/test/java/com/espark/adarsh/ApplicationMainTests.java
@@ -1,17 +1,12 @@
 package com.espark.adarsh;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@Slf4j
-@RunWith(SpringRunner.class)
 public class ApplicationMainTests {
 
 
-     @Test
+    @Test
     public void test(){
 
-     }
+    }
 }
